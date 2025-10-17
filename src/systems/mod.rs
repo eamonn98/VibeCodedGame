d0a1efbeaa26bea2eab0b95b6a334b03a9fa5a80
Eamonn <eamonn98@gmail.com>
2025-10-17 14:46:11 +0100

    Trim unused system re-exports

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -5,10 +5,10 @@ mod combat;
 mod movement;
 mod physics;
 
-pub use physics::{CollisionLayers, PhysicsDebugSettings, PhysicsPlugin, PIXELS_PER_METER};
+pub use physics::{CollisionLayers, PhysicsDebugSettings, PIXELS_PER_METER};
 
 pub use combat::{ComboTracker, EnemyDeathEvent, EnemyHitEvent};
-pub use movement::{MovementState, MovementSystemSet};
+pub use movement::MovementState;
 
 pub struct SystemsPlugin;
 
